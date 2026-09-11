f2cd128028cab62e2f12b18050629afcd7a3a91b
Raihan Alyfan Sardi <90737998+Terraquantum@users.noreply.github.com>
2026-09-11 19:44:17 +0700

    tester2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,10 @@
                 Send message
                 <ArrowUpIcon />
             </Button>
-            
+            <Button class="mt-5">
+                Send message
+                <ArrowUpIcon />
+            </Button>
         </div>
     </div>
 </div>
